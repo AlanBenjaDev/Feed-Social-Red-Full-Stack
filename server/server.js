f2501fa47372a3b0e68c86d49712746668c1c92e
AlanBenjaDev <alenciopalencio@gmail.com>
2025-08-08 16:48:48 -0300

    solucion final

--- server/server.js
+++ server/server.js
@@ -11,12 +11,6 @@ const comentarioRouter = require('./comentario.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const conecction = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
-});
 
 app.use(cors());
 app.use(express.json());
